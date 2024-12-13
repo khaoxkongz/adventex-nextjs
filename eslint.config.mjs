ca6d194ae0c6cfa6e20a61f154348e0ccdd76081
khaoxkongz <thanapat.makkawal@gmail.com>
2024-12-13 15:47:13 +0700

    Update ESLint configuration and package dependencies
    - Added Tailwind CSS plugin to ESLint configuration for improved linting support.
    - Updated ESLint rules and settings to accommodate TypeScript files and Tailwind CSS usage.
    - Introduced new linting script for automatic fixes in package.json.
    - Added new dependency for ESLint and TypeScript parser versions.
    
    These changes enhance code quality and maintainability in the project.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path"
 import { fileURLToPath } from "url"
 import { FlatCompat } from "@eslint/eslintrc"
+import tailwindcss from "eslint-plugin-tailwindcss"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -10,7 +11,41 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier",
+    "plugin:tailwindcss/recommended"
+  ),
+  {
+    plugins: {
+      tailwindcss,
+    },
+  },
+  { ignores: ["**/components/magicui/**"] },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@next/next/no-html-link-for-pages": "off",
+      "tailwindcss/no-custom-classname": "off",
+      "tailwindcss/classnames-order": "error",
+    },
+  },
+  {
+    settings: {
+      tailwindcss: {
+        callees: ["cn", "cva"],
+        config: "tailwind.config.ts",
+      },
+      next: {
+        rootDir: ["app/*/"],
+      },
+    },
+  },
+  {
+    files: ["*.ts", "*.tsx"],
+    parser: "@typescript-eslint/parser",
+  },
 ]
 
 export default eslintConfig
