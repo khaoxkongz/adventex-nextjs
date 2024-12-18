0ccc0429e5118127f7f21825d4baed35b30d13b2
khaoxkongz <thanapat.makkawal@gmail.com>
2024-12-18 10:03:20 +0700

    Refactor Prettier configuration by replacing `.prettierrc` with `prettier.config.cjs`
    - Removed the old `.prettierrc` file and introduced a new `prettier.config.cjs` file to standardize code formatting settings.
    - Updated import order to include new paths for `~/env` and `~/data`, enhancing module organization.
    - Maintained existing formatting rules and plugins for consistency across the project.
    
    These changes aim to improve code formatting management and maintainability.

---
+++ prettier.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  importOrder: [
+    "^(react/(.*)$)|^(react$)",
+    "^(next/(.*)$)|^(next$)",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^~/env/(.*)$",
+    "^~/types/(.*)$",
+    "^~/config/(.*)$",
+    "^~/lib/(.*)$",
+    "^~/hooks/(.*)$",
+    "^~/components/ui/(.*)$",
+    "^~/components/(.*)$",
+    "^~/emails/(.*)$",
+    "^~/data/(.*)$",
+    "^~/assets/(.*)$",
+    "^~/styles/(.*)$",
+    "",
+    "^[./]",
+  ],
+  importOrderCaseSensitive: true,
+  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+}
